1c06dd350308186e57a6e725db3f31856b8a7db4
BlakeLewis1 <blakelewis110@gmail.com>
2020-06-11 09:00:50 +0000

    jt

--- s4/application/routes.py
+++ s4/application/routes.py
@@ -16,7 +16,7 @@ def password():
     password = " "
     
     randWords = random.choices(words, k = 5) # new array of words in random order, k is number of items
-    for i in range(len(randWords)): # 
+    for i in range(len(randWords)): #
         password += randWords[i] # pulls each item from array and store it as password,
     
     return password
